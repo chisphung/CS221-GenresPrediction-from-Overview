b0f6eac5437d4bf701b15f136e7845026be4f8ed
phungminhchi2k5 <phungminhchi2005@gmail.com>
2025-06-30 21:53:52 +0700

    remove pos

--- datasets/download.py
+++ datasets/download.py
@@ -2,6 +2,6 @@ import gdown
 import os
 
 if __name__ == "__main__":
-    output_path = "datasets/preprocessed_dataset.py"
+    output_path = "datasets/preprocessed_dataset.csv"
     gdown.download("https://drive.google.com/uc?id=1-9XtB-Nn_1xinwpRug68O-WLFuSThhOo", output_path, quiet=False)
     print("Dataset downloaded successfully to:", output_path)
