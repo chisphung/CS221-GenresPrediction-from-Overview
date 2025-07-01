b7a824f95e1eb11ac421aa65ff260da3d94b8a76
Nguyen Huu Minh Chien <162682495+chiennhm@users.noreply.github.com>
2025-07-01 20:54:57 +0700

    fix bugs trim_datasets

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -72,6 +72,36 @@ def preprocess_text(text):
     text = lemmatize_words(text)
     return text
 
+def trim_dataset(df_train, max_per_genre=20000, preserve_labels = []):
+    genre_cols = df_train.columns[1:]
+    genre_counter = {genre: 0 for genre in genre_cols}
+        
+    df_shuffled = df_train.sample(fraction=1.0, shuffle=True, seed=42)
+    kept_rows = []
+    
+    for row in df_shuffled.iter_rows(named=True):
+        genres = [genre for genre in genre_cols if row[genre] == 1]
+        if any(label in preserve_labels for label in genres):
+            kept_rows.append(row)
+            for genre in genres:
+                genre_counter[genre] += 1
+    
+    for row in df_shuffled.iter_rows(named=True):
+        genres = [genre for genre in genre_cols if row[genre] == 1]
+        if row in kept_rows:
+            continue
+         
+        if any(label in preserve_labels for label in genres):
+            continue
+         
+        if all(genre_counter[genre] < max_per_genre for genre in genres):
+            kept_rows.append(row)
+            for genre in genres:
+                genre_counter[genre] += 1
+    
+    df_trimmed = pl.DataFrame(kept_rows)
+    return df_trimmed
+
 if __name__ == "__main__":
     # Load the dataset
     # dataset1 = load_dataset('wykonos/movies')
@@ -105,38 +135,6 @@ if __name__ == "__main__":
     # print(len(df_preprocessed['overview']))
     df_preprocessed.write_csv("datasets/preprocessed_dataset.csv")
     # Trim the dataset
-
-    def trim_dataset(df_train, max_per_genre=20000, preserve_labels = []):
-        
-      genre_cols = df_train.columns[1:]
-      genre_counter = {genre: 0 for genre in genre_cols}
-        
-      df_shuffled = df_train.sample(fraction=1.0, shuffle=True, seed=42)
-      kept_rows = []
-    
-      for row in df_shuffled.iter_rows(named=True):
-          genres = [genre for genre in genre_cols if row[genre] == 1]
-          if any(label in preserve_labels for label in genres):
-              kept_rows.append(row)
-              for genre in genres:
-                  genre_counter[genre] += 1
-    
-      for row in df_shuffled.iter_rows(named=True):
-          genres = [genre for genre in genre_cols if row[genre] == 1]
-          if row in kept_rows:
-              continue
-         
-          if any(label in preserve_labels for label in genres):
-              continue
-         
-          if all(genre_counter[genre] < max_per_genre for genre in genres):
-              kept_rows.append(row)
-              for genre in genres:
-                  genre_counter[genre] += 1
-    
-      df_trimmed = pl.DataFrame(kept_rows)
-      return df_trimmed
-
-df_trimmed = trim_dataset(df_preprocessed, max_per_genre = 20000, preserve_labels=['War', 'History', 'Western'])
-df_trimmed.write_csv("datasets/trimmed_dataset.csv")
+    df_trimmed = trim_dataset(df_preprocessed, max_per_genre = 20000, preserve_labels=['War', 'History', 'Western'])
+    df_trimmed.write_csv("datasets/trimmed_dataset.csv")
 
