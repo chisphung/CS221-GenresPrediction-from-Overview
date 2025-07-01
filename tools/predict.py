76122704b0ec35e15a691aef9f0d197f70807e7b
phungminhchi2k5 <phungminhchi2005@gmail.com>
2025-07-01 12:58:36 +0700

    add eval

--- tools/predict.py
+++ tools/predict.py
@@ -3,8 +3,14 @@ import torch
 import numpy as np 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import sys
+from load_model import load_model
+from data_loader import get_data_loader
+import pandas as pd
+import json 
 
-def get_predictions(model, data_loader, device):
+genres = json.load(open('../datasets/id2genre.json', 'r'))
+
+def get_predictions(model, data_loader, device="cuda" if torch.cuda.is_available() else "cpu"):
     """
     Outputs:
       predictions - 
@@ -41,3 +47,29 @@ def get_predictions(model, data_loader, device):
     target_values = torch.stack(target_values)
     
     return titles, predictions, prediction_probs, target_values
+
+if __name__ == "__main__":
+  if len(sys.argv) < 3:
+       print("Usage: python predict.py <model_path> <data>")
+       sys.exit(1)
+
+  model_path = sys.argv[1]
+  data = sys.argv[2]
+
+  model, tokenizer = load_model(model_path)
+  data_loader = get_data_loader(data, tokenizer)
+
+  titles, predictions, prediction_probs, target_values = get_predictions(model, data_loader)
+  print("Predictions completed.")
+  print(f"Predictions: {predictions}")
+  for index, pred in enumerate(predictions):
+     print(f"Sample {index + 1}:")
+     result = []
+     for genre_index, genre in enumerate(pred):
+        if genre == 1:
+           result.append(genres[str(genre_index)])
+     print(result)
+           
+        
+
+   # Process and save the predictions as needed
