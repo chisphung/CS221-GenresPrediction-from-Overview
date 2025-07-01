8abfaf21b817c1de3fc940f742c8551d810a0721
Nguyen Huu Minh Chien <162682495+chiennhm@users.noreply.github.com>
2025-07-01 20:50:17 +0700

    update trim_datasets

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -106,21 +106,37 @@ if __name__ == "__main__":
     df_preprocessed.write_csv("datasets/preprocessed_dataset.csv")
     # Trim the dataset
 
-    max_per_genre = 45000
+    def trim_dataset(df_train, max_per_genre=20000, preserve_labels = []):
+        
+      genre_cols = df_train.columns[1:]
+      genre_counter = {genre: 0 for genre in genre_cols}
+        
+      df_shuffled = df_train.sample(fraction=1.0, shuffle=True, seed=42)
+      kept_rows = []
+    
+      for row in df_shuffled.iter_rows(named=True):
+          genres = [genre for genre in genre_cols if row[genre] == 1]
+          if any(label in preserve_labels for label in genres):
+              kept_rows.append(row)
+              for genre in genres:
+                  genre_counter[genre] += 1
+    
+      for row in df_shuffled.iter_rows(named=True):
+          genres = [genre for genre in genre_cols if row[genre] == 1]
+          if row in kept_rows:
+              continue
+         
+          if any(label in preserve_labels for label in genres):
+              continue
+         
+          if all(genre_counter[genre] < max_per_genre for genre in genres):
+              kept_rows.append(row)
+              for genre in genres:
+                  genre_counter[genre] += 1
+    
+      df_trimmed = pl.DataFrame(kept_rows)
+      return df_trimmed
 
-    genre_cols = df_preprocessed.columns[1:]  # Remove 'overview' col
-    genre_counter = {genre: 0 for genre in genre_cols}
+df_trimmed = trim_dataset(df_train, max_per_genre = 20000, preserve_labels=['War', 'History', 'Western'])
+df_trimmed.write_csv("datasets/trimmed_dataset.csv")
 
-    kept_rows = []
-
-    for row in df_preprocessed.iter_rows(named=True):
-        genres = [genre for genre in genre_cols if row[genre] == 1]
-
-        if all(genre_counter[genre] < max_per_genre for genre in genres):
-            kept_rows.append(row)
-            for genre in genres:
-                genre_counter[genre] += 1
-
-    df_trimmed = pl.DataFrame(kept_rows)
-    # Save the preprocessed and trimmed dataset
-    df_trimmed.write_csv("datasets/preprocessed_trimmed.csv")
