c5219013a9bbb40da5a9861f7bfb3d9b1e1b5f40
chisphung <phungminhchi2005@gmail.com>
2025-07-02 14:58:52 +0700

    add ensemble and test_emsenlble

--- streamlit.py
+++ streamlit.py
@@ -9,49 +9,57 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.main import single_predict
 from tools.load_model import load_model
 from tools.postprocess import post_process
+from tools.ensemble import ensemble_predict
+import json
 
 # Mapping from label index to genre
-ID_TO_GENRE = {
-    0: 'Action', 1: 'Adventure', 2: 'Animation', 3: 'Comedy', 4: 'Crime',
-    5: 'Documentary', 6: 'Drama', 7: 'Family', 8: 'Fantasy', 9: 'History',
-    10: 'Horror', 11: 'Music', 12: 'Mystery', 13: 'Romance', 14: 'Science Fiction',
-    15: 'TV Movie', 16: 'Thriller', 17: 'War', 18: 'Western'
-}
-
+genres_bert_cased = json.load(open("datasets/id2genre_bert_cased.json"))
+genres_bert_uncased = json.load(open("datasets/id2genre_bert_uncased.json"))
+genres_distilled_bert = json.load(open("datasets/id2genre_distillBERT.json"))
 def main():
     st.title("🎬 Movie Genre Prediction from Overview")
     
     text = st.text_area("Enter the movie overview:")
     model_selection = st.selectbox("Choose a base model:", [
         "bert-base-uncased",
-        "distilled-bert-base-uncased",
-        "roberta-base"
+        "distilbert-base-uncased",
+        "bert-base-cased",
+        "ensemble"
     ])
     if model_selection == "bert-base-uncased":
-        model_path = "weights/bert_based.pt"
-    elif model_selection == "distilled-bert-base-uncased":
-        model_path = "weights/disstilledBERT.pt"
-    elif model_selection == "roberta-base":
-        model_path = "weights/roberta_base.pt"
+        ID_TO_GENRE = genres_bert_uncased
+        model_path = "weights/bert_uncased.pt"
+    elif model_selection == "distilbert-base-uncased":
+        ID_TO_GENRE = genres_distilled_bert
+        model_path = "weights/distilled_bert.pt"
+    elif model_selection == "bert-base-cased":
+        ID_TO_GENRE = genres_bert_cased
+        model_path = "weights/bert_cased.pt"
 
     if st.button("Predict Genres"):
         if not text.strip():
             st.warning("Please enter some text to classify.")
             return
-        
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        model, tokenizer = load_model(model_path, device)
+        if model_selection == "ensemble":
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            preds = ensemble_predict(text, device)
+            st.success("Prediction Complete")
+            st.markdown("### Predicted Genres:")
+            st.write(preds)
+        else:
+            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            model, tokenizer = load_model(model_path, device, base_model=model_selection)
 
-        preds, probs = single_predict(model, tokenizer, text, device)
-        top_preds = post_process(probs, k=3)
-        
-        st.success("Prediction Complete")
-        st.markdown("### Top Predicted Genres:")
-        for i, pred in enumerate(top_preds[0]):
-            if pred == 1:
-                genre = ID_TO_GENRE[i]
-                prob = probs[0][i].item()
-                st.write(f"{genre}: {prob:.2f}")
+            preds, probs = single_predict(model, tokenizer, text, device)
+            top_preds = post_process(probs, k=3)
+            
+            st.success("Prediction Complete")
+            st.markdown("### Top Predicted Genres:")
+            for i, pred in enumerate(top_preds[0]):
+                if pred == 1:
+                    genre = ID_TO_GENRE[str(i)]
+                    prob = probs[0][i].item()
+                    st.write(f"{genre}: {prob:.2f}")
 
 
 if __name__ == "__main__":
--- tools/logistic_regression.py
+++ tools/logistic_regression.py
@@ -5,29 +5,31 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 
-df = pl.read_csv("datasets/train.csv")
-df = df.filter(~(pl.col('genres').is_null() | pl.col('overview').is_null()))
-df = df.select(['overview', 'genres'])
+if __name__ == "__main__":
 
-df = df.copy()
-labels = df.iloc[:, 1:-1].values
-df['labels'] = list(labels)
+    df = pl.read_csv("datasets/train.csv")
+    df = df.filter(~(pl.col('genres').is_null() | pl.col('overview').is_null()))
+    df = df.select(['overview', 'genres'])
 
-corpus = list(df['overview'].dropna())
-tfidf = TfidfVectorizer()
-vectors = tfidf.fit_transform(corpus).toarray()
+    df = df.copy()
+    labels = df.iloc[:, 1:-1].values
+    df['labels'] = list(labels)
 
-df['overview'] = list(vectors)
-df = df.loc[:, ['overview','labels']]
+    corpus = list(df['overview'].dropna())
+    tfidf = TfidfVectorizer()
+    vectors = tfidf.fit_transform(corpus).toarray()
 
-TEST_SIZE = 0.2
-split_idx = int(TEST_SIZE * len(vectors))
-Trainvecs = vectors[:split_idx]
-Testvecs = vectors[split_idx:]
-Trainclss = labels[:split_idx]
-Testclss = labels[split_idx:]
+    df['overview'] = list(vectors)
+    df = df.loc[:, ['overview','labels']]
 
-cls = MultiOutputClassifier(LogisticRegression())
-cls.fit(Trainvecs, Trainclss)
+    TEST_SIZE = 0.2
+    split_idx = int(TEST_SIZE * len(vectors))
+    Trainvecs = vectors[:split_idx]
+    Testvecs = vectors[split_idx:]
+    Trainclss = labels[:split_idx]
+    Testclss = labels[split_idx:]
 
-joblib.dump(cls, './weights/logistic_regression_model.joblib')
+    cls = MultiOutputClassifier(LogisticRegression())
+    cls.fit(Trainvecs, Trainclss)
+
+    joblib.dump(cls, './weights/logistic_regression_model.joblib')
